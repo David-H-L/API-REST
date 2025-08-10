b1b217ffedae43d7a46e8f83061a2d81d5c64649
David Huanca Ledezma <davidhuancaledezma@gmail.com>
2025-08-09 23:34:14 -0400

    feat: add model customer

---
+++ src/main/java/com/project/cine/models/CustomerModel.java
@@ -0,0 +1,22 @@
+package com.project.cine.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customer")
+@PrimaryKeyJoinColumn(name = "id")
+
+public class CustomerModel extends UserModel {
+
+    @Column(nullable = false)
+    private int customerPoints;
+
+    public int getCustomerPoints() {
+        return customerPoints;
+    }
+
+    public void setCustomerPoints(int customerPoints) {
+        this.customerPoints = customerPoints;
+    }
+
+}
