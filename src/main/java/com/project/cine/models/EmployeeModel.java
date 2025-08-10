2cc1fa8d78671fa7ca45a3c2b643caf71762afdb
David Huanca Ledezma <davidhuancaledezma@gmail.com>
2025-08-09 23:34:40 -0400

    feat: add model employee

---
+++ src/main/java/com/project/cine/models/EmployeeModel.java
@@ -0,0 +1,47 @@
+package com.project.cine.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee")
+@PrimaryKeyJoinColumn(name = "id")
+
+public class EmployeeModel extends UserModel {
+
+    @Column(nullable = false)
+    private double salary;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
